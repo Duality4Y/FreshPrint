442d99c9531e87938f517a558bc5e74d0df5e909
Robert van der Tuuk <robertvdtuuk@gmail.com>
2022-06-19 23:42:52 +0200

    initial commit lets hope I didn't forget anything.

---
+++ freshPrint.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import os
+import sys
+import time
+import argparse
+import subprocess
+import validators
+import configparser
+
+config = None
+
+imagePrefix = "fresh_image_"
+
+def StoreImages(images, output):
+	print(f"storing images in {output}")
+	for i, image in enumerate(images):
+		imageIdentifier = f"{imagePrefix}{i}.png"
+		imageFilePath = os.path.join(output, imageIdentifier)
+
+		print(f"Storing image as: {imageFilePath}")
+		with open(imageFilePath, "wb") as f:
+			f.write(image)
+
+def ViewImage(path):
+	subprocess.Popen(['xdg-open', path])
+
+def ViewImages(images, path):
+	StoreImages(images, path)
+	for i in range(0, len(images)):
+		imageFilePath = os.path.join(path, f"{imagePrefix}{i}.png")
+		print(f"Launching viewer for image: {imageFilePath}")
+		ViewImage(imageFilePath)
+
+def FindElementClass(driver, className):
+	elements = driver.find_elements_by_xpath('//*[@class]')
+	for element in elements:
+		try:
+			attr = element.get_attribute('class')
+			if attr == className:
+				return element
+		except Exception as e:
+			pass # just ignore the 'StaleElementReferenceException'
+
+def GetElementImage(driver, element):
+	return element.screenshot_as_png
+
+def GetRecipe(url):
+	""" Load the needed class descriptions for elements of interest. """
+	elementClasses = []
+	for key in config['HelloFresh']:
+		value = config['HelloFresh'][key]
+		elementClasses.append(value)
+	
+	options = Options()
+	options.headless = True
+
+	driver = webdriver.Firefox(options=options)
+	driver.get(url)
+	
+	# accept the cookie.
+	CookieAcceptId = config['Cookie']['CookieElemId']
+	WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.ID, CookieAcceptId))).click()
+	
+	# images that are dynamically loaded need to be scrolled into view.
+	body = driver.find_element_by_css_selector('body')
+	for i in range(0, 10):
+		body.send_keys(Keys.PAGE_DOWN)
+		time.sleep(0.7)
+	
+	imageResults = []
+	for elementClass in elementClasses:
+		element = FindElementClass(driver, elementClass)
+		elementImage = GetElementImage(driver, element)
+		imageResults.append(elementImage)
+	
+	driver.quit()
+
+	return imageResults
+
+def SendToPrinter(images):
+	""" Generate printing options from config file."""
+	printCommandOpts = []
+	for key in config['LinuxPrintingOpts']:
+		value = config['LinuxPrintingOpts'][key]
+		subOptStr = f"{key}={value}" if value else f"{key}"
+		printCommandOpts.append("-o " + subOptStr)
+	print(f"lpr command-line options: {printCommandOpts}")
+	
+	""" Send images to the printer. """
+	printCommand = ['lpr', *printCommandOpts]
+	print(f"command: {printCommand}")
+	for image in images:
+		lpr = subprocess.Popen(printCommand, stdin=subprocess.PIPE)
+		stdout, stderr = lpr.communicate(input=image)
+
+def LoadConfig(configFile):
+	config = configparser.ConfigParser()
+	config.read(configFile)
+	return config
+
+def ValidateHelloFreshUrl(url):
+	validUrl = bool(validators.url(url))
+	hellofreshUrl = "hellofresh" in url
+
+	if not (validUrl and hellofreshUrl):
+		raise argparse.ArgumentTypeError
+	
+	return url
+
+def CheckConfigExists(config):
+	if os.path.exists(config):
+		return config
+	
+	raise argparse.ArgumentTypeError(f"No such file or directory, {config}")
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Download and Print Hello-Fresh recipes.")
+	
+	parser.formatter_class = lambda prog: argparse.RawTextHelpFormatter(prog, max_help_position=30)
+
+	parser.add_argument('-u', '--url',
+						type=ValidateHelloFreshUrl,
+						default="",
+						required=True,
+						help="recipe url")
+	parser.add_argument('-c', '--config',
+						type=CheckConfigExists,
+						default="Config.ini",
+						required=False,
+						help="Config to load, default is Config.ini")
+	parser.add_argument('--print',
+						default=False,
+						required=False,
+						action='store_true',
+						help="send a printjob.")
+	parser.add_argument('--show',
+						default=False,
+						required=False,
+						action='store_true',
+						help="launch a system image viewer.")
+	parser.add_argument('-o', '--output',
+						default="/tmp",
+						required=False,
+						help="set the output location for images.")
+	parser.add_argument('--verbose',
+						required=False,
+						action="store_true",
+						help="enable verbose printing.")
+
+	args = parser.parse_args()
+
+	# if verbose then print everything to stdout
+	if not args.verbose:
+		print = lambda *args, **kwargs: None
+	
+	print(args)
+
+	print(f"Hello-fresh url received:	"f"{args.url}")
+	print(f"Using Config:				"f"{args.config}")
+	print(f"Send printjob:				"f"{args.print}")
+	print(f"Launch image view:			"f"{args.show}")
+	print(f"Output images to:			"f"{args.output}")
+
+	scriptFilePath = os.path.realpath(__file__)
+	scriptPath = os.path.dirname(scriptFilePath)
+
+	configFilename = args.config
+	configFilePath = os.path.join(scriptPath, configFilename)
+
+	print("scriptFilePath:	"f"{scriptFilePath}")
+	print("scriptPath:		"f"{scriptPath}")
+	print("configFilename:	"f"{configFilename}")
+	print("configFilePath:	"f"{configFilePath}")
+
+	config = LoadConfig(configFilePath)
+
+	images = GetRecipe(args.url)
+
+	if args.print:
+		print("Sending a recipe print job.")
+		SendToPrinter(images)
+	else:
+		print("Not sending a recipe print job.")
+	
+	if args.show:
+		print("Launching image viewer.")
+		ViewImages(images, args.output)
+	else:
+		print("Not showing images.")
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Description
+
+This tool allows you to download recipes from hello-fresh so that you can print them out.
+
+# Capabilities
+using selenium this script connects to a hello fresh website does some magic
+then creates some snapshots of the relevant elements on the page 
+depending on the command line options shows you the result or sends a print job.
+
+potentially in the future the script could be made to also generate only textually output it isn't hard to add.
+
+## command-line usage
+```
+usage: freshPrint.py [-h] -u URL [-c CONFIG] [--print] [--show] [-o OUTPUT] [--verbose]
+
+Download and Print Hello-Fresh recipes.
+
+optional arguments:
+  -h, --help                  show this help message and exit
+  -u URL, --url URL           recipe url
+  -c CONFIG, --config CONFIG  Config to load, default is Config.ini
+  --print                     send a printjob.
+  --show                      launch a system image viewer.
+  -o OUTPUT, --output OUTPUT  set the output location for images.
+  --verbose                   enable verbose printing.
+```
+
+# Config
+This program looks for a config file name Config.ini
+if one is not present then make sure to copy ExampleConfig.ini and rename it Config.ini
+
+## [HelloFresh]
+The values in this section are search for on the webpage and taken a screenshot of.
+Having these in a config file allows to edit these if they do ever change on the webpage (idem for the cookie button element).
+
+## [Cookie]
+The site shows a cookie banner this section indicates what element to click in order to just accept them.
+
+## [LinuxPrintingOpts]
+These options translate to ``lpr`` command-line options pretty much 1 to 1
+they will generate the -o options for that command.
+
+# Requirements
+the following packages are required:
+ - python3
+ - selenium
+ - validators
+ - gecko-driver (apt install firefox-geckodriver)
